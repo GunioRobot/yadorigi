9aba45133e633a140a171dd716c70fd831d0d107
kazuhiko Sakaguchi <pi8027@gmail.com>
2010-05-31 23:38:01 +0900

    write kind inference

--- compiler/Typing/KindInference.hs
+++ compiler/Typing/KindInference.hs
@@ -1,5 +1,7 @@
 
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances,
+             UndecidableInstances,
+             OverlappingInstances #-}
 
 module Yadorigi.Typing.KindInference where
 
@@ -49,39 +51,89 @@ unify (VarKind n _) kind = addSubst n kind
 unify kind (VarKind n _) = addSubst n kind
 unify _ _ = lift $ Left KindInferenceError
 
+unify' :: Kind -> Kind -> KindInferenceMonad ()
+unify' AstKind AstKind = return ()
+unify' (FuncKind f a) (FuncKind g b) = unify' f g >> unify' a b
+unify' (VarKind n _) kind = addSubst n kind >> return ()
+unify' kind (VarKind n _) = addSubst n kind >> return ()
+unify' _ _ = lift $ Left KindInferenceError
+
 match :: Kind -> Kind -> KindInferenceMonad Kind
 match AstKind AstKind = return AstKind
 match (FuncKind f a) (FuncKind g b) = liftM2 FuncKind (match f g) (match a b)
 match (VarKind n _) kind = addSubst n kind
 match _ _ = lift $ Left KindInferenceError
 
---class KindInference a where
---    unifyAll :: a -> KindInferenceMonad ()
---
---instance (Traversable f,KindInference a) => KindInference (f a) where
---    unifyAll = mapM_ unifyAll
---
---instance KindInference Module where
---    unifyAll (Module _ _ _ decls) = unifyAll decls
---
---instance KindInference Decl where
---    unifyAll (Decl _ _ decl) = unifyAll decl
---
---instance KindInference PrimDecl where
---    unifyAll (DataDecl _ _ _ body) = unifyAll $ map snd body
---    unifyAll (TypeDecl _ _ typename) = unifyAll typename
---    unifyAll (ClassDecl _ _ _ decls) = unifyAll decls
---    unifyAll (InstanceDecl _ _ typename decls) = unifyAll typename >> unifyAll decls
---    unifyAll (BindDecl bind whereClause) = unifyAll bind >> unifyAll whereClause
---    unifyAll _ = return ()
---
---instance KindInference Bind where
---    unifyAll (Bind lhs rhs) = unifyAll lhs >> unifyAll rhs
---
---instance KindInference Lhs where
---    unifyAll
---
---class KindInference' a where
---    unifyAll' :: a -> KindInferenceMonad Kind
+match' :: Kind -> Kind -> KindInferenceMonad ()
+match' AstKind AstKind = return ()
+match' (FuncKind f a) (FuncKind g b) = match' f g >> match' a b
+match' (VarKind n _) kind = addSubst n kind >> return ()
+match' _ _ = lift $ Left KindInferenceError
 
+class KindInference a where
+    unifyAll :: a -> KindInferenceMonad ()
+
+class KindInference' a where
+    unifyAll' :: a -> KindInferenceMonad Kind
+
+instance (Traversable f,KindInference a) => KindInference (f a) where
+    unifyAll = mapM_ unifyAll
+
+instance KindInference Module where
+    unifyAll (Module _ _ _ decls) = unifyAll decls
+
+instance KindInference Decl where
+    unifyAll (Decl _ decl) = unifyAll decl
+
+instance KindInference PrimDecl where
+    unifyAll (DataDecl _ _ _ body) = unifyAll $ map snd body
+    unifyAll (TypeDecl _ _ typename) = unifyAll typename
+    unifyAll (ClassDecl _ _ _ decls) = unifyAll decls
+    unifyAll (InstanceDecl _ _ typename decls) = unifyAll typename >> unifyAll decls
+    unifyAll (BindDecl _ bind whereClause) = unifyAll bind >> unifyAll whereClause
+    unifyAll _ = return ()
+
+instance KindInference Bind where
+    unifyAll (Bind lhs rhs) = unifyAll lhs >> unifyAll rhs
+
+instance KindInference Lhs where
+    unifyAll (FunctionLhs _ param) = unifyAll param
+    unifyAll (InfixLhs _ pat1 pat2) = unifyAll pat1 >> unifyAll pat2
+    unifyAll (PatternLhs pat) = unifyAll pat
+
+instance KindInference Rhs where
+    unifyAll (ExprRhs expr) = unifyAll expr
+    unifyAll (GuardRhs guard) = unifyAll guard
+
+instance KindInference Guard where
+    unifyAll (Guard cond expr) = unifyAll cond >> unifyAll expr
+
+instance KindInference Expr where
+    unifyAll (Expr _ expr) = unifyAll expr
+
+instance KindInference PrimExpr where
+    --unifyAll (LiteralExpr _) = return ()
+    --unifyAll (NameExpr _) = return ()
+    unifyAll (ApplyExpr expr1 expr2) = unifyAll expr1 >> unifyAll expr2
+    unifyAll (InfixExpr _ left right) = unifyAll left >> unifyAll right
+    unifyAll (NegativeExpr expr) = unifyAll expr
+    unifyAll (ParenthesesExpr expr) = unifyAll expr
+    unifyAll (ListExpr exprs) = unifyAll exprs
+    unifyAll (LambdaExpr lambda) = unifyAll lambda
+    unifyAll (LetExpr _ lets expr) = unifyAll lets >> unifyAll expr
+    unifyAll (IfExpr c t f) = unifyAll c >> unifyAll t >> unifyAll f
+    unifyAll (CaseExpr expr pat) = unifyAll expr >> unifyAll pat
+    unifyAll (TypeSignatureExpr expr typename) =
+        unifyAll expr >> unifyAll' typename >>= unify' AstKind
+    unifyAll _ = return ()
+
+instance KindInference Lambda where
+    unifyAll (Lambda _ _ param expr) = unifyAll param >> unifyAll expr
+
+instance KindInference CasePattern where
+    unifyAll (CasePattern _ pat rhs) = unifyAll pat >> unifyAll rhs
+
+
+instance KindInference' a => KindInference a where
+    unifyAll a = unifyAll' a >> return ()
 
